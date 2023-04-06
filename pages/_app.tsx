0fe352bc3e45774ad6ec049a003ebedb3c7a4435
Kelvin Ochubili <kelvinochubili@gmail.com>
2023-04-06 14:49:46 +0100

    installed sass and set up style files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "../styles/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="Empowering the planet with the latest in computer technology"
+        />
+        <meta name="keywords" content="Keywords" />
+        <title>Bitpiler</title>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,48 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="application-name" content="Bitpiler" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Bitpiler" />
+        <meta
+          name="description"
+          content="Empowering the planet with the latest in computer technology"
+        />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://app.colabb.io" />
+        <meta name="twitter:title" content="Bitpiler" />
+        <meta
+          name="twitter:description"
+          content="Empowering the planet with the latest in computer technology"
+        />
+        {/* <meta name="twitter:image" content="https://staging.app.colabb.io/svg/icon.svg" /> */}
+        <meta name="twitter:creator" content="@iykekelvins" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Bitpiler" />
+        <meta
+          property="og:description"
+          content="Empowering the planet with the latest in computer technology"
+        />
+        <meta property="og:site_name" content="Bitpiler" />
+        <meta property="og:url" content="https://app.colabb.io" />
+        {/* <meta property="og:image" content="https://staging.app.colabb.io/svg/icon.svg" /> */}
+
+        {/* <link rel="manifest" href="/manifest.json" /> */}
+        <link href="/favicon.svg" rel="icon" type="image/svg" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/icon-16.png"></link>
+        {/* <meta name="theme-color" content="#1b1464" /> */}
+      </Head>
+      <body>
+        <div id="modal"></div>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
