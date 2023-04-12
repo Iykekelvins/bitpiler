5a9c41c6fbf5c746a95d6fa7ea1a75b8bc3038ca
Kelvin Ochubili <kelvinochubili@gmail.com>
2023-04-12 18:37:04 +0100

    home responsiveness update

--- components/Home/Services.tsx
+++ components/Home/Services.tsx
@@ -6,20 +6,22 @@ import c from "./Home.module.scss";
 const Services = () => {
   return (
     <section className={c.home_services}>
-      {services.map((item) => (
-        <div key={item.title} className={c.home_services_item}>
-          <Image
-            src="/assets/images/product.png"
-            width={360}
-            height={392}
-            alt={`${item.title} desc image`}
-          />
-          <div className={c.home_services_item_footer}>
-            <h3>{item.title}</h3>
-            <p data-splitting="lines">{item.info}</p>
-          </div>
-        </div>
-      ))}
+      <ul>
+        {services.map((item) => (
+          <li key={item.title} className={c.home_services_item}>
+            <Image
+              src="/assets/images/product.png"
+              width={360}
+              height={392}
+              alt={`${item.title} desc image`}
+            />
+            <div className={c.home_services_item_footer}>
+              <h3>{item.title}</h3>
+              <p data-splitting="lines">{item.info}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
