96a558583056ab5a3c859e4e6dd5056a3c341476
Kelvin Ochubili <kelvinochubili@gmail.com>
2023-04-13 18:04:41 +0100

    coming soon page done

--- components/ComingSoon/index.tsx
+++ components/ComingSoon/index.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
 import c from "./ComingSoon.module.scss";
+import Buttons from "@/shared/Buttons";
 
 const ComingSoon = () => {
-  return <div className={c.soon}></div>;
+  useEffect(() => {
+    window.document.body.style.backgroundColor = "#000a20";
+  }, []);
+  return (
+    <div className={c.soon}>
+      <div className={c.soon_content}>
+        <h4 className="white">COMING SOON</h4>
+        <h1 className="white">
+          Get Notified when <br /> we Launch
+        </h1>
+        <p>
+          With the right mentors, and challenging projects, any skill can be
+          mastered.{" "}
+        </p>
+
+        <div className={c.soon_content_input}>
+          <input type="text" placeholder="Enter Email Address" />
+          <Buttons title="Notify me" started />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ComingSoon;
