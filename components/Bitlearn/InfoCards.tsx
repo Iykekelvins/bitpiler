ff5b4a660530a2fdd44c92a27b6a5c3304c0d9c1
Kelvin Ochubili <kelvinochubili@gmail.com>
2023-04-29 17:53:42 +0100

    animations on text for bitlearn cards

--- components/Bitlearn/InfoCards.tsx
+++ components/Bitlearn/InfoCards.tsx
@@ -9,12 +9,16 @@ const InfoCards = () => {
   return (
     <ul className={c.bitlearn_cards}>
       {cards.map((card) => (
-        <li className={c.bitlearn_cards_card} key={card.title_1}>
+        <li
+          className={c.bitlearn_cards_card}
+          key={card.title_1}
+          data-animation="text"
+        >
           <Image src={card.img} height={700} width={480} alt="info image" />
           <div className={c.bitlearn_cards_card_info}>
-            <h4>{card.title_1}</h4>
-            <h2>{card.title_2}</h2>
-            <p>{card.info}</p>
+            <h4 data-splitting="chars">{card.title_1}</h4>
+            <h2 data-splitting="words">{card.title_2}</h2>
+            <p data-splitting="words">{card.info}</p>
             <div className={c.bitlearn_cards_card_btns}>
               <Link href="/coming-soon">
                 <Buttons title="Class Details" arrow started />
