3b3c586245eb651d0cd830b9a7837baf4554a6e0
Kelvin Ochubili <kelvinochubili@gmail.com>
2023-05-05 21:07:33 +0100

    cleanups

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,13 +15,13 @@ export default function Document() {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://app.colabb.io" />
+        <meta name="twitter:url" content="https://bitpiler.vercel.app/" />
         <meta name="twitter:title" content="Bitpiler" />
         <meta
           name="twitter:description"
           content="Empowering the planet with the latest in computer technology"
         />
-        {/* <meta name="twitter:image" content="https://staging.app.colabb.io/svg/icon.svg" /> */}
+        {/* <meta name="twitter:image" content="https://https://bitpiler.vercel.app//svg/icon.svg" /> */}
         <meta name="twitter:creator" content="@iykekelvins" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Bitpiler" />
@@ -30,8 +30,8 @@ export default function Document() {
           content="Empowering the planet with the latest in computer technology"
         />
         <meta property="og:site_name" content="Bitpiler" />
-        <meta property="og:url" content="https://app.colabb.io" />
-        {/* <meta property="og:image" content="https://staging.app.colabb.io/svg/icon.svg" /> */}
+        <meta property="og:url" content="https://bitpiler.vercel.app/" />
+        {/* <meta property="og:image" content="https://https://bitpiler.vercel.app//svg/icon.svg" /> */}
 
         {/* <link rel="manifest" href="/manifest.json" /> */}
         <link href="/favicon.svg" rel="icon" type="image/svg" sizes="16x16" />
